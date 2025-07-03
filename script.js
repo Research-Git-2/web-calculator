e4b2a5f5363c35478dea6a793ebfe1cf8e8d48d3
Den <dend51108@gmail.com>
2025-07-03 22:41:56 +0300

    + истроия, -стиль истории

--- script.js
+++ script.js
@@ -4,45 +4,16 @@ let firstNumber = "";
 let secondNumber = "";
 let operator = "";
 
-const num7 = () => {
-  appendNumber("7");
-};
-
-const num8 = () => {
-  appendNumber("8");
-};
-
-const num9 = () => {
-  appendNumber("9");
-};
-
-const num4 = () => {
-  appendNumber("4");
-};
-
-const num5 = () => {
-  appendNumber("5");
-};
-
-const num6 = () => {
-  appendNumber("6");
-};
-
-const num1 = () => {
-  appendNumber("1");
-};
-
-const num2 = () => {
-  appendNumber("2");
-};
-
-const num3 = () => {
-  appendNumber("3");
-};
-
-const num0 = () => {
-  appendNumber("0");
-};
+const num7 = () => appendNumber("7");
+const num8 = () => appendNumber("8");
+const num9 = () => appendNumber("9");
+const num4 = () => appendNumber("4");
+const num5 = () => appendNumber("5");
+const num6 = () => appendNumber("6");
+const num1 = () => appendNumber("1");
+const num2 = () => appendNumber("2");
+const num3 = () => appendNumber("3");
+const num0 = () => appendNumber("0");
 
 const appendNumber = (num) => {
   if (operator === "") {
@@ -57,6 +28,13 @@ const appendNumber = (num) => {
 const numD = () => {
   if (firstNumber !== "" && operator === "") {
     operator = "/";
+    display.textContent = firstNumber + " " + operator;
+  }
+};
+
+const numPer = () => {
+  if (firstNumber !== "" && operator === "") {
+    operator = "%";
     display.textContent = firstNumber + " " + operator;
   }
 };
@@ -111,27 +89,61 @@ const calculate = () => {
         }
         result = num1 / num2;
         break;
-
       case "+":
         result = num1 + num2;
         break;
-
       case "-":
         result = num1 - num2;
         break;
-
       case "*":
         result = num1 * num2;
         break;
-
+      case "%":
+        result = (num1 * num2) / 100;
+        break;
       default:
         return;
     }
 
+    result = parseFloat(result.toFixed(4));
     display.textContent = result;
+    addToHistory(`${num1} ${operator} ${num2}`, result);
     firstNumber = result.toString();
     secondNumber = "";
     operator = "";
+  }
+};
+
+const root = () => {
+  if (firstNumber !== "" && operator === "") {
+    const num = parseFloat(firstNumber);
+
+    if (num < 0) {
+      display.textContent = "Ошибка: √ из отрицательного";
+      reset();
+      return;
+    }
+
+    const result = Math.sqrt(num);
+    display.textContent = result;
+    addToHistory(`√${num}`, result);
+    firstNumber = result.toString();
+  } else {
+    display.textContent = "Ошибка: сначала введите число";
+    reset();
+  }
+};
+
+const pow = () => {
+  if (firstNumber !== "" && operator === "") {
+    const num = parseFloat(firstNumber);
+    const result = Math.pow(num, 2);
+    display.textContent = result;
+    addToHistory(`${num}²`, result);
+    firstNumber = result.toString();
+  } else {
+    display.textContent = "Ошибка: сначала введите число";
+    reset();
   }
 };
 
@@ -139,56 +151,33 @@ const reset = () => {
   firstNumber = "";
   secondNumber = "";
   operator = "";
-  display.textContent = "";
+  display.textContent = "0";
 };
 
-const seven = document.querySelector("#seven");
-seven.addEventListener("click", num7);
+const addToHistory = (expr, result) => {
+  const historyList = document.getElementById("historyList");
+  const li = document.createElement("li");
+  li.textContent = `${expr} = ${result}`;
+  historyList.prepend(li);
+};
 
-const eight = document.querySelector("#eight");
-eight.addEventListener("click", num8);
-
-const nine = document.querySelector("#nine");
-nine.addEventListener("click", num9);
-
-const four = document.querySelector("#four");
-four.addEventListener("click", num4);
-
-const five = document.querySelector("#five");
-five.addEventListener("click", num5);
-
-const six = document.querySelector("#six");
-six.addEventListener("click", num6);
-
-const one = document.querySelector("#one");
-one.addEventListener("click", num1);
-
-const two = document.querySelector("#two");
-two.addEventListener("click", num2);
-
-const three = document.querySelector("#three");
-three.addEventListener("click", num3);
-
-const numZero = document.querySelector("#zero");
-numZero.addEventListener("click", num0);
-
-const divide = document.querySelector("#D");
-divide.addEventListener("click", numD);
-
-const numSum = document.querySelector("#plus");
-numSum.addEventListener("click", sum);
-
-const numMinus = document.querySelector("#minus");
-numMinus.addEventListener("click", minus);
-
-const numMult = document.querySelector("#mult");
-numMult.addEventListener("click", mult);
-
-const dotBtn = document.querySelector("#point");
-dotBtn.addEventListener("click", point);
-
-const equal = document.querySelector("#equal");
-equal.addEventListener("click", calculate);
-
-const clear = document.querySelector("#AC");
-clear.addEventListener("click", reset);
+document.querySelector("#pow").addEventListener("click", pow);
+document.querySelector("#root").addEventListener("click", root);
+document.querySelector("#seven").addEventListener("click", num7);
+document.querySelector("#eight").addEventListener("click", num8);
+document.querySelector("#nine").addEventListener("click", num9);
+document.querySelector("#four").addEventListener("click", num4);
+document.querySelector("#five").addEventListener("click", num5);
+document.querySelector("#six").addEventListener("click", num6);
+document.querySelector("#one").addEventListener("click", num1);
+document.querySelector("#two").addEventListener("click", num2);
+document.querySelector("#three").addEventListener("click", num3);
+document.querySelector("#zero").addEventListener("click", num0);
+document.querySelector("#D").addEventListener("click", numD);
+document.querySelector("#per").addEventListener("click", numPer);
+document.querySelector("#plus").addEventListener("click", sum);
+document.querySelector("#minus").addEventListener("click", minus);
+document.querySelector("#mult").addEventListener("click", mult);
+document.querySelector("#point").addEventListener("click", point);
+document.querySelector("#equal").addEventListener("click", calculate);
+document.querySelector("#AC").addEventListener("click", reset);
